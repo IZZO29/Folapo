a8bf7296e7cf656fe0fe911cbfe245003cec59cc
Priyanshu Dhoke <dhoke.priyanshu2002@gmail.com>
2023-07-03 23:39:07 +0530

    second commit

--- client/src/screens/SignUp.js
+++ client/src/screens/SignUp.js
@@ -8,7 +8,7 @@ export default function () {
     const handleSubmit = async(e)=>{
         e.preventDefault();
         console.log(JSON.stringify({name:credentials.name, email:credentials.email, password:credentials.password, location:credentials.geolocation}))
-        const response = await fetch("http://localhost:5000/api/createuser",{
+        const response = await fetch("https://folapo.onrender.com/api/createuser",{
             method:'POST',
             headers:{
                 'Content-Type':'application/json'
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 5000
 const mongoDB = require("./db")
 const cors = require('cors')
+const path = require('path')
 app.use(cors())
 
 app.use((req,res,next)=>{
@@ -24,4 +25,11 @@ app.use('/api', require("./Routes/DisplayData"));
 app.use('/api', require("./Routes/OrderData"));
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+})
+
+//static
+app.use(express.static(path.join(__dirname, './client/build')))
+
+app.get('*', function(req,res){
+  res.sendFile(path.join(__dirname, "./client/build"))
 })
