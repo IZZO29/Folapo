3ed740bcae8938d817e946340de5ea08bceb8f4c
Priynshu <52713855+IZZO29@users.noreply.github.com>
2023-07-04 22:08:21 +0530

    MyOrder.js

--- client/src/screens/MyOrder.js
+++ client/src/screens/MyOrder.js
@@ -8,7 +8,7 @@ export default function MyOrder() {
 
     const fetchMyOrder = async () => {
         console.log(localStorage.getItem('userEmail'))
-        await fetch("https://folapo.onrender.com/api/myOrderData", {
+        await fetch("https://folapooo.onrender.com/api/myOrderData", {
             // credentials: 'include',
             // Origin:"http://localhost:3000/login",
             method: 'POST',
