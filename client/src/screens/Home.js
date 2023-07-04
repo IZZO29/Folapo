c848f3a90c2f71388f80050503c15ac92afbab36
Priynshu <52713855+IZZO29@users.noreply.github.com>
2023-07-04 22:07:49 +0530

    Home.js

--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -11,7 +11,7 @@ export default function Home(){
   const [foodItem,setFoodItem] = useState([]);
   
   const loadData = async ()=>{
-    let response = await fetch("https://folapo.onrender.com/api/foodData",{
+    let response = await fetch("https://folapooo.onrender.com/api/foodData",{
       method:"POST",
       header:{
         'Content-Type': 'application/json'
